fe1b0f11c7083e01a5b469b2a3f98b3417f46036
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-09 20:10:01 +0900

    format

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -61,4 +61,3 @@ fun DarkPreview() {
         MyApp()
     }
 }
-
--- app/src/main/java/com/example/androiddevchallenge/ui/timer/CountDonwCircleView.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/timer/CountDonwCircleView.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.timer
 
 import android.util.Log
@@ -6,9 +21,7 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -17,7 +30,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,30 +59,33 @@ fun CountDownCircle(viewModel: TimerViewModel, modifier: Modifier, circleSize: D
         currentAngleDegree.toFloat()
     }
     Box {
-        Canvas(modifier = modifier
-            .padding(Padding)
+        Canvas(
+            modifier = modifier
+                .padding(Padding)
 //            .background(Color.Blue)
-            .fillMaxWidth(), onDraw = {
-            drawCircle(
-                color = Color(0x8028252C),
-                center = Offset(x = circleSize.toPx() / 2, y = circleSize.toPx() / 2),
-                radius = size.minDimension / 2,
-                style = Stroke(STROKE)
-            )
-            drawArc(
-                Color(0xFFF2A337),
-                -90f,
-                degree,
-                useCenter = false,
-                size = Size(
-                    circleSize.toPx(),
-                    circleSize.toPx()
-                ),
+                .fillMaxWidth(),
+            onDraw = {
+                drawCircle(
+                    color = Color(0x8028252C),
+                    center = Offset(x = circleSize.toPx() / 2, y = circleSize.toPx() / 2),
+                    radius = size.minDimension / 2,
+                    style = Stroke(STROKE)
+                )
+                drawArc(
+                    Color(0xFFF2A337),
+                    -90f,
+                    degree,
+                    useCenter = false,
+                    size = Size(
+                        circleSize.toPx(),
+                        circleSize.toPx()
+                    ),
 //            topLeft = Offset(60f, 60f),
-                style = Stroke(STROKE),
+                    style = Stroke(STROKE),
 //                blendMode = BlendMode.SrcOver
-            )
-        })
+                )
+            }
+        )
     }
 }
 
@@ -79,4 +94,3 @@ fun CountDownCircle(viewModel: TimerViewModel, modifier: Modifier, circleSize: D
 fun PreviewMyCircle() {
 //    CountDownCircle()
 }
-
